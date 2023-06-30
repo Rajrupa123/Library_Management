6b1cd2bb695f7099208953f8919e7afe8f9dbc99
Rajrupa Roy <rajruparoy1@gmail.com>
2023-06-30 14:56:19 +0530

    last commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost/library_db");
+mongoose.connect("mongodb+srv://rajruparoy1:kWOn1dj418qRUVt3@cluster0.wrzjtix.mongodb.net/");
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"Error while connecting to mongoose"));
 db.once("open",function(params) {
