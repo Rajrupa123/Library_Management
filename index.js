221cc46563335cfa4d32a81feb5db1be30c63eee
Rajrupa Roy <rajruparoy1@gmail.com>
2023-06-30 11:01:40 +0530

    Last commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port =process.env.port|| 8000;
+const port =8000;
 
 const db = require('./config/mongoose');
 // used for session cookie
